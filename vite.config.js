b6fdbe6a2a90ea9bdb11a96a3102e4c8919d69f1
Cameron Thomas <CamThomas47@gmail.com>
2025-11-29 13:46:36 -0500

    configuring vite config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import { NodePackageImporter } from "sass-embedded";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        importers: [new NodePackageImporter()]
-      }
-    }
-  }
-})
+	plugins: [react()],
+	build: {
+		outDir: "build",
+	},
+	server: {
+		host: true,
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				importers: [new NodePackageImporter()],
+			},
+		},
+	},
+});
