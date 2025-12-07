94ec760bff133f1d4a9126edac67ce199b25d7f6
Cameron Thomas <CamThomas47@gmail.com>
2025-12-07 16:55:17 -0500

    contact form completed

--- frontend/src/components/ButtonPrimary.tsx
+++ frontend/src/components/ButtonPrimary.tsx
@@ -3,15 +3,16 @@ import React from "react";
 interface ButtonPrimaryProps {
 	label: string;
 	action?: () => void;
-	type: string;
+	type: 'submit' | 'reset' | 'button';
 	size: string;
 	outline?: boolean;
 	dark: boolean
 }
 
-const ButtonPrimary = ({ label, action, type, size, outline, dark }: ButtonPrimaryProps): React.JSX.Element => {
+const ButtonPrimary = ({ label, action, type = 'submit', size, outline, dark }: ButtonPrimaryProps): React.JSX.Element => {
 	return (
 		<button
+			type={type}
 			onClick={action}
 			className="bg-primary font-semibold py-2 px-8 rounded-full text-black text-md shadow-md hover:bg-white hover:outline-primary hover:outline duration-100 lg:w-60">
 			{label}
